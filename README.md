bc029ff5edabe3ce2e4d6eb4a33e267aa8691aa8
Nilesh Kumar <nilesh.guptain@gmail.com>
2022-08-16 20:40:12 +0530

    the base

---
+++ README.md
@@ -0,0 +1,6 @@
+# Domains For Hackers. By Hackers
+
+This project does domain service on a Polygon L2. It comes with a simple contract(which is what I initially thought), a test for that contract(that I might never get around to keeping up-to-date), and a script that deploys that contract(coz who does all this work on their own when we have computers).
+
+Try buying some domains, you!
+
---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.10",
+};
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
