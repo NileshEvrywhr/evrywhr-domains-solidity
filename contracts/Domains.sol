8e516b91a3fcd8c21baea44b7557f94a7a0ef9d2
Nilesh Kumar <nilesh.guptain@gmail.com>
2022-08-16 23:49:55 +0530

    adds domains, resolvers, and records

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -4,7 +4,39 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. BY HACKERS. FOR HACKERS.");
-  }
+
+    // stores what domain owned by which user address
+    mapping(string => address) public domains;
+
+    // store what domains point to values
+    mapping(string => string) public records;
+
+
+    constructor() {
+        console.log("THIS IS A DOMAINS CONTRACT. BY HACKERS. FOR HACKERS.");
+    }
+
+    // adds domain to user address in mapping
+    function register(string calldata name) public {
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has claimed .%s", msg.sender, name);
+    }
+
+    // searches domain owner's address
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        // only owner can set his domain records
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns(string memory) {
+        return records[name];
+    }
+
+
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,21 @@
 const main = async () => {
+    // owner is deployer, randomPerson to test access control
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    const txn = await domainContract.register("hacks");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("hacks");
+    console.log("Owner of domain .hacks:", domainOwner);
+
+    // trying to set a record that doesn't belong to randomPerson
+    txn = await domainContract.connect(randomPerson).setRecord("hacks", "personal web2 domain");
+    await txn.wait();
   };
   
   const runMain = async () => {
