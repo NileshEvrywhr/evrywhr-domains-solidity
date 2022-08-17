92bae2d201b11f104550e62af39f85dd126b2064
Nilesh Kumar <nilesh.guptain@gmail.com>
2022-08-17 13:20:12 +0530

    Deploy to Mumbai, see on OpenSea

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,12 @@ require("@nomiclabs/hardhat-ethers");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/BorlYu1LUxWZwA1Fk4RDr0huyNZHuGjp",
+      accounts: ["08cab42a60c76adfd4dd1f3f6b211e2646165b244fec05479d7d923708829b8c"]
+    }
+  }
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
--- scripts/run.js
+++ scripts/run.js
@@ -1,21 +1,26 @@
 const main = async () => {
     // owner is deployer, randomPerson to test access control
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("hacker");
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
+    // console.log("Contract deployed by:", owner.address);
 
-    const txn = await domainContract.register("hacks");
+    // passing value, second variable is moneyyyyyyyyyyy
+    const txn = await domainContract.register("mortal", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("hacks");
-    console.log("Owner of domain .hacks:", domainOwner);
+    const address = await domainContract.getAddress("mortal");
+    console.log("Owner of domain mortal:", address);
 
     // trying to set a record that doesn't belong to randomPerson
-    txn = await domainContract.connect(randomPerson).setRecord("hacks", "personal web2 domain");
-    await txn.wait();
+    // txn = await domainContract.connect(randomPerson).setRecord("hacks", "personal web2 domain");
+    // await txn.wait();
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
+
   };
   
   const runMain = async () => {
