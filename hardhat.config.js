cd8a4d322952bfb35d3f9d7e9c094c73552c8c3c
Nilesh Kumar <nilesh.guptain@gmail.com>
2022-08-18 23:36:55 +0530

    moves sensitive info to dotenv file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-chai-matchers");
 require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.10",
   networks: {
     mumbai: {
-      url: "https://polygon-mumbai.g.alchemy.com/v2/BorlYu1LUxWZwA1Fk4RDr0huyNZHuGjp",
-      accounts: ["08cab42a60c76adfd4dd1f3f6b211e2646165b244fec05479d7d923708829b8c"]
-    }
-  }
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
